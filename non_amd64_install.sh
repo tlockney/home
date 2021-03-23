868a1f0b6f659dc93850a9c94ce23292bd02201d
Thomas Lockney <thomas@lockney.net>
2021-03-22 22:48:57 -0700

    Fixed error trying to install pyenv if it's already there

--- non_amd64_install.sh
+++ non_amd64_install.sh
@@ -1,4 +1,11 @@
 sudo apt update
 sudo apt install -y build-essential curl emacs mosh tmux zsh
 
-curl https://pyenv.run | bash
+# Install pipx
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+# Install pyenv
+if [ ! -d $HOME/.pyenv ]; then
+    curl https://pyenv.run | bash
+fi
