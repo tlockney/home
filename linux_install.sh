46691766ba1bcc9cd28c96fccd63826f95abae60
Thomas Lockney <thomas@lockney.net>
2021-05-16 23:03:25 +0000

    More packages and Processing install

--- linux_install.sh
+++ linux_install.sh
@@ -1,3 +1,12 @@
+# util functions
+
+function get_download_url {
+    wget -q -nv -O- https://api.github.com/repos/$1/$2/releases/latest 2>/dev/null |  jq -r '.assets[] | select(.browser_download_url | contains("linux64")) | .browser_download_url'
+}
+
+# Versions (where needed)
+GO_VERSION=1.16.4
+
 # Add necessary repositories
 
 ## vscode
@@ -26,7 +35,16 @@ sudo apt install -y \
     zsh \
     openjdk-11-jdk \
     htop \
-    tree
+    tree \
+    libreadline-gplv2-dev \
+    libncursesw5-dev \
+    libssl-dev \
+    libsqlite3-dev \
+    tk-dev \
+    libgdbm-dev \
+    libc6-dev \
+    libbz2-dev \
+    jq
 
 # Install Visual Studio Code
 sudo apt install -y code
@@ -47,6 +65,26 @@ python3 -m pipx ensurepath
 if [ ! -d $HOME/.pyenv ]; then
     curl https://pyenv.run | bash
 fi 
+
+# Install Go
+GO_PLATFORM=linux-amd64
+GO_FILE=go$GO_VERSION.$GO_PLATFORM.tar.gz
+wget https://storage.googleapis.com/golang/$GO_FILE
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+rm $GO_FILE
+
+# Install Processing
+sudo mkdir -p /opt/Processing
+pushd /opt/Processing
+PROCESSING_DL=$(get_download_url processing processing)
+PROCESSING_FILE=$(echo $PROCESSING_DL | cut -d'/' -f9)
+sudo wget $PROCESSING_DL
+PROCESSING_DIR=$(tar tzf processing-3.5.4-linux64.tgz | head -1 | sed -e 's@/.*@@')
+sudo tar xzvf $PROCESSING_FILE
+sudo rm $PROCESSING_FILE
+sudo ln -sf $PROCESSING_DIR processing
+popd
+/opt/Processing/processing/install.sh 
 
 # Remove some unnecessary packages
 sudo apt remove -y chromium-browser
