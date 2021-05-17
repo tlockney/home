bc4b79db3d11eb6df5fa1fc1e5aad190175e8b2f
Thomas Lockney <thomas@lockney.net>
2021-05-17 16:14:15 -0700

    tweaked Jupyter setup

--- run_once_2_pipx_install.sh
+++ run_once_2_pipx_install.sh
@@ -5,6 +5,7 @@ PIPX=$(which pipx)
 if [[ -x "$PIPX" ]]; then
     $PIPX install --force jupyterlab || true
     $PIPX inject jupyterlab requests pandas numpy
+    $PIPX inject --include-apps jupyterlab notebook
     $PIPX install --force ptpython || true
     $PIPX install --force pygments || true
     $PIPX install --force httpie || true
